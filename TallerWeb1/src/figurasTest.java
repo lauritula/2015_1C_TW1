7422c57fccb9b5d359c16d22dbf98bd2f4ee7858
DavidGonzaloCastillo <david_stsm@hotmail.com>
2015-04-15 15:06:29 -0300

    EjercicioTarea
    EjercicioTarea

---
+++ TallerWeb1/src/figurasTest.java
@@ -0,0 +1,13 @@
+public class figurasTest {
+	
+	public static void main(String args[]){
+		
+		Triangulo equilatero=new Triangulo(10,10);
+		
+		equilatero.areaTriangulo();
+		
+		equilatero.perimetroTriangulo();
+		
+		
+	}
+}
